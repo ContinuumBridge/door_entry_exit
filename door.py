705eb7bba1a618d2bc8e4fa025302545e6d0e398
PeterClaydon <peter.claydon@continuumbridge.com>
2015-08-18 10:57:53 +0100

    First working version

--- door.py
+++ door.py
@@ -7,8 +7,11 @@
 # Default values:
 config = {
     "entry-exit": True,
-    "alert": False,
-    "ignore_time": 120
+    "in_pir_to_door_time": 30,
+    "door_close_to_in_pir_time": 10,
+    "door_open_to_in_pir_time": 30,
+    "max_door_open_time": 60,
+    "data_send_delay": 1
 }
 
 import sys
@@ -19,41 +22,9 @@ from cbconfig import *
 import requests
 import json
 from twisted.internet import reactor
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
-CONFIG_FILE                       = CB_CONFIG_DIR + "hot_drinks.config"
+CONFIG_FILE                       = CB_CONFIG_DIR + "door_entry_exit.config"
 CID                               = "CID164"  # Client ID
-
-def betweenTimes(t, t1, t2):
-    # True if epoch t is between times of day t1 and t2 (in 24-hour clock format: "23:10")
-    t1secs = (60*int(t1.split(":")[0]) + int(t1.split(":")[1])) * 60
-    t2secs = (60*int(t2.split(":")[0]) + int(t2.split(":")[1])) * 60
-    stamp = time.strftime("%Y %b %d %H:%M", time.localtime(t)).split()
-    today = stamp
-    today[3] = "00:00"
-    today_e = time.mktime(time.strptime(" ".join(today), "%Y %b %d %H:%M"))
-    yesterday_e = today_e - 24*3600
-    #print "today_e: ", today_e, "yesterday_e: ", yesterday_e
-    tt1 = [yesterday_e + t1secs, today_e + t1secs]
-    tt2 = [yesterday_e + t2secs, today_e + t2secs]
-    #print "tt1: ", tt1, " tt2: ", tt2
-    smallest = 50000
-    decision = False
-    if t - tt1[0] < smallest and t - tt1[0] > 0:
-        smallest = t - tt1[0]
-        decision = True
-    if t - tt2[0] < smallest and t -tt2[0] > 0:
-        smallest = t - tt2[0]
-        decision = False
-    if t - tt1[1] < smallest and t -tt1[1] > 0:
-        smallest = t - tt1[1]
-        decision = True
-    if t - tt2[1] < smallest and t - tt2[1] > 0:
-        smallest = t - tt2[1]
-        decision = False
-    return decision
 
 def nicetime(timeStamp):
     localtime = time.localtime(timeStamp)
@@ -61,43 +32,105 @@ def nicetime(timeStamp):
     now = time.strftime('%H:%M:%S, %d-%m-%Y', localtime)
     return now
 
-class HotDrinks():
-    def __init__(self, bridge_id):
-        self.bridge_id = bridge_id
-        self.on = False
-        self.offTime = 0
+class EntryExit():
+    def __init__(self):
+        self.inside_triggered = False
+        self.inside_pir_on = False
+        self.door_open = False
+        self.action = "nothing"
+        self.checkExit = {}
+        self.inside_pir_on_time = 0
+        self.inside_pir_off_time = 0
+        self.inside_pir_on = False
+        self.door_open = False
+        self.door_open_time = 0
+        self.door_close_time = 0
+        self.state = "idle"
+        self.pirID = None
+        self.magID = None
+        self.bridge_id = None
+        self.idToName = {}
         self.s = []
         self.waiting = False
-        self.power = False
-        self.binary = False
+        reactor.callLater(10, self.fsm)
 
-    def setNames(self, idToName):
-        self.idToName = idToName
+    def onChange(self, devID, timeStamp, value):
+        if devID == self.magID:
+            sensor = "magsw"
+        elif devID == self.pirID:
+            sensor = "pir"
+        else:
+            sensor = "unknown"
+        self.cbLog("debug", "EntryExit, onChange. sensor: " + sensor)
+        if sensor == "pir":
+            if value == "on":
+                self.inside_pir_on_time = timeStamp
+                self.inside_pir_on = True
+            else:
+                self.inside_pir_off_time = timeStamp
+                self.inside_pir_on = False
+        if sensor == "magsw":
+            if value == "on":
+                self.door_open = True
+                self.door_open_time = timeStamp
+            else:
+                self.door_open = False
+                self.door_close_time = timeStamp
 
-    def onChange(self, sensor, timeStamp, value):
-        try:
-            #self.cbLog("debug", "onChange. value: " + value + " self.binary: " + str(self.binary) + " config_alert: " + str(config["alert"]))
-            if ((self.power and value > config["threshold"]) or (self.binary and value == "on")) and not self.on:
-                if timeStamp - self.offTime > config["ignore_time"]:
-                    self.on = True
-                    if config["alert"]:
-                        msg = {"m": "alert",
-                               "a": "Hot drinks being made at " + nicetime(timeStamp) + " (by " + self.idToName[sensor] + ")",
-                               "t": timeStamp
-                              }
-                        self.client.send(msg)
-                        self.cbLog("debug", "msg send to client: " + str(json.dumps(msg, indent=4)))
-                    values = {
-                        "name": self.bridge_id + "/hot_drinks/" + sensor,
-                        "points": [[int(timeStamp*1000), 1]]
-                    }
-                    self.storeValues(values)
-            elif ((self.power and value < config["threshold"]) or (self.binary and value == "off"))  and self.on:
-                self.on = False
-                self.offTime = timeStamp
-                self.cbLog("debug", "HotDrinks off")
-        except Exception as ex:
-            self.cbLog("warning", "HotDrinks onChange encountered problems. Exception: " + str(type(ex)) + str(ex.args))
+    def fsm(self):
+        # This method is called every second
+        prev_state = self.state
+        action = "none"
+        if self.state == "idle":
+            if self.door_open:
+                if self.door_open_time - self.inside_pir_on_time < config["in_pir_to_door_time"] or self.inside_pir_on:
+                    self.state = "check_going_out"
+                else:
+                    self.state = "check_coming_in"
+        elif self.state == "check_going_out":
+            if not self.door_open:
+                self.state = "check_went_out"
+        elif self.state == "check_went_out":
+            t = time.time()
+            if t - self.door_close_time > config["door_close_to_in_pir_time"]:
+                if self.inside_pir_on or t - self.inside_pir_off_time < config["door_close_to_in_pir_time"] - 4:
+                    action = "answered_door"
+                    self.state = "idle"
+                else:
+                    action = "went_out"
+                    self.state = "idle"
+        elif self.state == "check_coming_in":
+            if self.inside_pir_on:
+                action = "came_in"
+                self.state = "wait_door_close"
+            elif time.time() - self.door_open_time > config["door_open_to_in_pir_time"]:
+                action = "open_and_close"
+                self.state = "wait_door_close"
+        elif self.state == "wait_door_close":
+            if not self.door_open:
+                self.state = "idle"
+            elif time.time() - self.door_open_time > config["max_door_open_time"]:
+                action = "door_open_too_long"
+                self.state = "wait_long_door_open"
+        elif self.state == "wait_long_door_open":
+            if not self.door_open:
+                self.state = "idle"
+        elif self.state == "wait_door_close":
+            if not self.door_open:
+                self.state = "idle"
+        else:
+            self.cbLog("warning", "self.door algorithm imposssible self.state")
+            self.state = "idle"
+        if self.state != prev_state:
+            self.cbLog("debug", "checkExits, new state: " + self.state)
+        if action != "none":
+            self.cbLog("debug", "checkExits, action: " + action) 
+            values = {
+                "name": self.bridge_id + "/entry_exit/" + self.idToName[self.magID] + "/" + action,
+                "points": [[int(time.time()*1000), 1]]
+            }
+            self.storeValues(values)
+        reactor.callLater(1, self.fsm)
 
     def sendValues(self):
         msg = {"m": "data",
@@ -120,10 +153,7 @@ class App(CbApp):
         self.state = "stopped"
         self.status = "ok"
         self.devices = []
-        self.devServices = [] 
         self.idToName = {} 
-        self.entryExitIDs = []
-        self.hotDrinkIDs = []
         #CbApp.__init__ MUST be called
         CbApp.__init__(self, argv)
 
@@ -150,7 +180,7 @@ class App(CbApp):
         self.client.receive(message)
 
     def onClientMessage(self, message):
-        #self.cbLog("debug", "onClientMessage, message: " + str(json.dumps(message, indent=4)))
+        self.cbLog("debug", "onClientMessage, message: " + str(json.dumps(message, indent=4)))
         global config
         if "config" in message:
             if "warning" in message["config"]:
@@ -168,27 +198,22 @@ class App(CbApp):
     def onAdaptorData(self, message):
         #self.cbLog("debug", "onAdaptorData, message: " + str(json.dumps(message, indent=4)))
         if message["characteristic"] == "binary_sensor":
-            self.hotDrinks.onChange(message["id"], message["timeStamp"], message["data"])
+            self.entryExit.onChange(message["id"], message["timeStamp"], message["data"])
 
     def onAdaptorService(self, message):
-        #self.cbLog("debug", "onAdaptorService, message: " + str(json.dumps(message, indent=4)))
+        self.cbLog("debug", "onAdaptorService, message: " + str(json.dumps(message, indent=4)))
         if self.state == "starting":
             self.setState("running")
-        self.devServices.append(message)
         serviceReq = []
-        power = False
-        biinary = False
         for p in message["service"]:
-            if p["characteristic"] == "power":
-                power = True
-                self.hotDrinks.power = True
-            elif p["characteristic"] == "binary_sensor":
-                binary = True
-                self.hotDrinks.binary = True
-        if power:
-            serviceReq.append({"characteristic": "power", "interval": 0})
-        elif binary:
-            serviceReq.append({"characteristic": "binary_sensor", "interval": 0})
+            if p["characteristic"] == "binary_sensor":
+                if "type" in p:
+                    if p["type"] == "pir":
+                        self.entryExit.pirID = message["id"]
+                        serviceReq.append({"characteristic": "binary_sensor", "interval": 0})
+                else:
+                    self.entryExit.magID = message["id"]
+                    serviceReq.append({"characteristic": "binary_sensor", "interval": 0})
         msg = {"id": self.id,
                "request": "service",
                "service": serviceReq}
@@ -228,10 +253,11 @@ class App(CbApp):
         self.client.onClientMessage = self.onClientMessage
         self.client.sendMessage = self.sendMessage
         self.client.cbLog = self.cbLog
-        self.hotDrinks = HotDrinks(self.bridge_id)
-        self.hotDrinks.cbLog = self.cbLog
-        self.hotDrinks.client = self.client
-        self.hotDrinks.setNames(idToName2)
+        self.entryExit = EntryExit()
+        self.entryExit.client = self.client
+        self.entryExit.cbLog = self.cbLog
+        self.entryExit.bridge_id = self.bridge_id
+        self.entryExit.idToName = self.idToName
         self.setState("starting")
 
 if __name__ == '__main__':
